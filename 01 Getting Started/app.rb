748e9bd41b31596bcb2c990c4a33db784e445d7c
zinara <ozojiez@gmail.com>
2017-05-19 16:27:46 +0200

    Update app.rb

--- 01 Getting Started/app.rb
+++ 01 Getting Started/app.rb
@@ -8,7 +8,7 @@ class GiftBasket < Sinatra::Base
   attr_reader :tokens
   API_KEY = ENV['API_KEY']
   API_SECRET = ENV['API_SECRET']
-  APP_URL = "jamie.ngrok.io"
+  APP_URL = "f937a025.ngrok.io"
 
   def initialize
     @tokens = {}
